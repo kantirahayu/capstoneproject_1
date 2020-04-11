c0d5f0030450d2554dc62f22ebbe35af03194238
Kanti Rahayu <60611326+kantirahayu@users.noreply.github.com>
2020-04-11 16:21:34 +0700

    First Python Notebook

---
+++ Capstone_1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# *Capstone Project*
+# 
+
+# This notebook only to prove my ability to connect the notebook to GitHub.
+
+# In[1]:
+
+
+import pandas as pd 
+import numpy as np 
+
+
+# In[2]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
